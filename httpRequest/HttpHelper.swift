7c9ca24640c680cab834c80cef3da8c737bb11e2
abdullahawara <abdulla@Abdulla-Hawara-NAT.local>
2017-09-13 15:27:37 +0200

    Initial commit

---
+++ httpRequest/HttpHelper.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  httpRequest
+//
+//  Created by abdulla hawara on 12.09.17.
+//  Copyright © 2017 TestObject. All rights reserved.
+//
+
+import Foundation
--- httpRequest/ViewController.swift
+++ httpRequest/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var endpointTextField: UITextField!
+    @IBOutlet weak var statusCodeTextField: UILabel!
+    @IBOutlet weak var responseContentLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        endpointTextField.delegate = self
+        
+        activityIndicator.hidesWhenStopped = true
     }
-
+    
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()    }
+    
+    @IBAction func get_button(_ sender: Any) {
+        
+        // code salad ;)
+        
+        if endpointTextField.text != ""
+        {
+            activityIndicator.startAnimating()
+            UIApplication.shared.beginIgnoringInteractionEvents()
+            
+            let url = URL (string: endpointTextField.text!)
+            statusCodeTextField.text = ""
+            responseContentLable.text = ""
+            let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                DispatchQueue.main.async() {
+                    if error == nil
+                    {
+                        self.statusCodeTextField.text = String((response as! HTTPURLResponse).statusCode)
+                        self.responseContentLable.text = String(data: data!, encoding: .ascii)
+                    } else {
+                        self.statusCodeTextField.text = String((error as! NSError).code)
+                        self.responseContentLable.text = String(describing: error)
+                    }
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.shared.endIgnoringInteractionEvents()
+                }
+            }
+            task.resume()
+        } else {
+            statusCodeTextField.text = ""
+            responseContentLable.text = "Endpoint field can't be empty!"
+        }
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
