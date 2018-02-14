fa4e981cead4a3fd86f8640c275fc90f9da1d385
danielpaulus <daniel.paulus@icloud.com>
2018-02-14 11:23:14 +0100

    Applauncher Integration Test App

---
+++ appLauncherTestApp/AppLauncherTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppLauncherTestApp
+//
+//  Created by Daniel Paulus on 12.02.18.
+//  Copyright © 2018 SauceLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
